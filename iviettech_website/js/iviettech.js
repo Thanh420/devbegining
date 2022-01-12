0d12afa3e053d79f44e9b95059413bc95987050b
Thanh420 <95669462+Thanh420@users.noreply.github.com>
2022-01-12 15:34:54 +0700

    Add files via upload

---
+++ iviettech_website/js/iviettech.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function thongbao() {
+alert("Bạn đã đăng ký thành công!");
+}
